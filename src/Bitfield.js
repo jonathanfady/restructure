b01eb187ad0b7109e92ba9d9291512c19a6c98ff
Jonathan FADY <jonathan.fady@alstomgroup.com>
2023-03-02 13:35:23 +0100

    Add an option to create a Bitfield with an Array type argument. Not fully tested on edge cases.

--- src/Bitfield.js
+++ src/Bitfield.js
@@ -1,4 +1,6 @@
-import {Base} from './Base.js';
+import { Base } from './Base.js';
+import { Array as ArrayT } from './Array.js';
+import { Number as NumberT } from './Number.js';
 
 export class Bitfield extends Base {
   constructor(type, flags = []) {
@@ -9,6 +11,23 @@ export class Bitfield extends Base {
 
   decode(stream) {
     const val = this.type.decode(stream);
+
+    if ((this.type instanceof ArrayT) && (this.type.type instanceof NumberT)) {
+      const res = {};
+      const bitlength = this.type.type.size() * 8;
+      for (let i = 0; i < this.flags.length; i++) {
+        const j = i % bitlength;
+        const flag = this.flags[i];
+        if (flag != null) {
+          res[flag] = !!(val[0] & (1 << j));
+        }
+        if (j == (bitlength - 1)) {
+          val.shift()
+        }
+      }
+
+      return res;
+    }
 
     const res = {};
     for (let i = 0; i < this.flags.length; i++) {
@@ -26,6 +45,28 @@ export class Bitfield extends Base {
   }
 
   encode(stream, keys) {
+    if ((this.type instanceof ArrayT) && (this.type.type instanceof NumberT)) {
+      const vals = [];
+      const bitlength = this.type.type.size() * 8;
+      let val = 0;
+      for (let i = 0; i < this.flags.length; i++) {
+        const j = i % bitlength;
+        const flag = this.flags[i];
+        if (flag != null) {
+          if (keys[flag]) { val |= (1 << j); }
+        }
+        if (j == (bitlength - 1)) {
+          vals.push(val);
+          val = 0;
+        }
+      }
+      if ((this.flags.length % bitlength) != 0) {
+        vals.push(val);
+      }
+
+      return this.type.encode(stream, vals);
+    }
+
     let val = 0;
     for (let i = 0; i < this.flags.length; i++) {
       const flag = this.flags[i];
--- test/Bitfield.js
+++ test/Bitfield.js
@@ -1,29 +1,93 @@
 import assert from 'assert';
-import {Bitfield, uint8, DecodeStream, EncodeStream} from 'restructure';
+import { Bitfield, uint8, uint16le, Array as ArrayT, DecodeStream, EncodeStream } from 'restructure';
 
-describe('Bitfield', function() {
-  const bitfield = new Bitfield(uint8, ['Jack', 'Kack', 'Lack', 'Mack', 'Nack', 'Oack', 'Pack', 'Quack']);
-  const JACK  = 1 << 0;
-  const KACK  = 1 << 1;
-  const LACK  = 1 << 2;
-  const MACK  = 1 << 3;
-  const NACK  = 1 << 4;
-  const OACK  = 1 << 5;
-  const PACK  = 1 << 6;
+describe('Bitfield', function () {
+  const JACK = 1 << 0;
+  const KACK = 1 << 1;
+  const LACK = 1 << 2;
+  const MACK = 1 << 3;
+  const NACK = 1 << 4;
+  const OACK = 1 << 5;
+  const PACK = 1 << 6;
   const QUACK = 1 << 7;
 
-  it('should have the right size', () => assert.equal(bitfield.size(), 1));
+  const RACK = 1 << 0;
+  const SACK = 1 << 1;
+  const TACK = 1 << 2;
+  const UACK = 1 << 3;
+  const VACK = 1 << 4;
+  const WACK = 1 << 5;
+  const XACK = 1 << 6;
+  const YACK = 1 << 7;
 
-  it('should decode', function() {
-    const buffer = new Uint8Array([JACK | MACK | PACK | NACK | QUACK]);
-    assert.deepEqual(
-      bitfield.fromBuffer(buffer),
-      {Jack: true, Kack: false, Lack: false, Mack: true, Nack: true, Oack: false, Pack: true, Quack: true}
-    );
-  });
+  const ZACK = 1 << 0;
+  describe('bitfield1', function () {
+    const bitfield1 = new Bitfield(uint8, ['Jack', 'Kack', 'Lack', 'Mack', 'Nack', 'Oack', 'Pack', 'Quack']);
+    it('should have the right size', () => assert.equal(bitfield1.size(), 1));
 
-  it('should encode', function() {
-    let buffer = bitfield.toBuffer({Jack: true, Kack: false, Lack: false, Mack: true, Nack: true, Oack: false, Pack: true, Quack: true});
-    assert.deepEqual(buffer, Buffer.from([JACK | MACK | PACK | NACK | QUACK]));
-  });
+    it('should decode', function () {
+      const buffer = new Uint8Array([JACK | MACK | PACK | NACK | QUACK]);
+      assert.deepEqual(
+        bitfield1.fromBuffer(buffer),
+        { Jack: true, Kack: false, Lack: false, Mack: true, Nack: true, Oack: false, Pack: true, Quack: true }
+      );
+    });
+
+    it('should encode', function () {
+      let buffer = bitfield1.toBuffer({ Jack: true, Kack: false, Lack: false, Mack: true, Nack: true, Oack: false, Pack: true, Quack: true });
+      assert.deepEqual(buffer, Buffer.from([JACK | MACK | PACK | NACK | QUACK]));
+    });
+  })
+
+  describe('bitfield2', function () {
+    const bitfield2 = new Bitfield(new ArrayT(uint8, 2), ['Jack', 'Kack', 'Lack', 'Mack', 'Nack', 'Oack', 'Pack', 'Quack', 'Rack', 'Sack', 'Tack', 'Uack', 'Vack', 'Wack', 'Xack', 'Yack']);
+
+    it('should have the right size', () => assert.equal(bitfield2.size(), 2));
+
+    it('should decode', function () {
+      const buffer = new Uint8Array([JACK | MACK | NACK | PACK | QUACK, RACK | SACK | UACK | VACK | XACK]);
+      assert.deepEqual(
+        bitfield2.fromBuffer(buffer),
+        {
+          Jack: true, Kack: false, Lack: false, Mack: true, Nack: true, Oack: false, Pack: true, Quack: true,
+          Rack: true, Sack: true, Tack: false, Uack: true, Vack: true, Wack: false, Xack: true, Yack: false
+        }
+      );
+    });
+
+    it('should encode', function () {
+      let buffer = bitfield2.toBuffer({
+        Jack: true, Kack: false, Lack: false, Mack: true, Nack: true, Oack: false, Pack: true, Quack: true,
+        Rack: true, Sack: true, Tack: false, Uack: true, Vack: true, Wack: false, Xack: true, Yack: false
+      });
+      assert.deepEqual(buffer, new Uint8Array([JACK | MACK | NACK | PACK | QUACK, RACK | SACK | UACK | VACK | XACK]));
+    });
+  })
+
+  describe('bitfield3', function () {
+    const bitfield3 = new Bitfield(new ArrayT(uint16le, 2), ['Jack', 'Kack', 'Lack', 'Mack', 'Nack', 'Oack', 'Pack', 'Quack', 'Rack', 'Sack', 'Tack', 'Uack', 'Vack', 'Wack', 'Xack', 'Yack', 'Zack']);
+
+    it('should have the right size', () => assert.equal(bitfield3.size(), 4));
+
+    it('should decode', function () {
+      const buffer = new Uint16Array([JACK | MACK | NACK | PACK | QUACK | (UACK << 8), ZACK]);
+      assert.deepEqual(
+        bitfield3.fromBuffer(buffer),
+        {
+          Jack: true, Kack: false, Lack: false, Mack: true, Nack: true, Oack: false, Pack: true, Quack: true,
+          Rack: false, Sack: false, Tack: false, Uack: true, Vack: false, Wack: false, Xack: false, Yack: false,
+          Zack: true
+        }
+      );
+    });
+
+    it('should encode', function () {
+      let buffer = bitfield3.toBuffer({
+        Jack: true, Kack: false, Lack: false, Mack: true, Nack: true, Oack: false, Pack: true, Quack: true,
+        Rack: false, Sack: false, Tack: false, Uack: true, Vack: false, Wack: false, Xack: false, Yack: false,
+        Zack: true
+      });
+      assert.deepEqual(buffer, new Uint8Array([JACK | MACK | NACK | PACK | QUACK, UACK, ZACK, 0]));
+    });
+  })
 });
